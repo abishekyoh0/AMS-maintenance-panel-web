e1b1d0e45b4286f4abd88dd5adbe66597c07568b
Abishek <abish.yoho@gmail.com>
2026-02-13 16:52:49 +0530

    correction from sathish

--- src/pages/EmergencyAlerts/EmergencyAlerts.tsx
+++ src/pages/EmergencyAlerts/EmergencyAlerts.tsx
@@ -61,8 +61,7 @@ export const ALERT_HISTORY: AlertHistory[] = [
 ];
 
 const priorityStyle = (priority: string) => {
-  if (priority === "High")
-    return { bg: `${COLORS.red}33`, color: COLORS.red };
+  if (priority === "High") return { bg: `${COLORS.red}33`, color: COLORS.red };
 
   if (priority === "Medium")
     return { bg: `${COLORS.orange}33`, color: COLORS.orange };
@@ -77,16 +76,19 @@ const statusStyle = (status: string) => {
   return { bg: "#6A728233", color: COLORS.secoundy_gray };
 };
 
-
 const EmergencyDashboard: React.FC = () => {
   return (
     <div className="space-y-6" style={{ color: COLORS.primary_white }}>
-
       <div>
-        <h1 className={`flex items-center gap-2 ${FONTSIZE[36]} ${FONTWEIGHT[700]}`}>
+        <h1
+          className={`flex items-center gap-2 ${FONTSIZE[36]} ${FONTWEIGHT[700]}`}
+        >
           <img src={Alert} alt="alert" /> Emergency Alerts
         </h1>
-        <p className={`mt-2 ${FONTSIZE[16]} ${FONTWEIGHT[400]}`} style={{ color: COLORS.secoundy_gray }}>
+        <p
+          className={`mt-2 ${FONTSIZE[16]} ${FONTWEIGHT[400]}`}
+          style={{ color: COLORS.secoundy_gray }}
+        >
           View emergency alerts requiring maintenance attention
         </p>
       </div>
@@ -95,23 +97,40 @@ const EmergencyDashboard: React.FC = () => {
         <div className="rounded-xl p-5 bg-linear-to-r from-[#FB2C3633] to-[#FF690033] border border-[#FF64674D]">
           <img src={Alert} alt="" />
           <p className={`my-2 ${FONTSIZE[30]} ${FONTWEIGHT[700]}`}>1</p>
-          <p className={`my-2 ${FONTSIZE[14]} ${FONTWEIGHT[400]}`} style={{ color: COLORS.red }}>Requires Attention</p>
+          <p
+            className={`my-2 ${FONTSIZE[14]} ${FONTWEIGHT[400]}`}
+            style={{ color: COLORS.red }}
+          >
+            Requires Attention
+          </p>
         </div>
 
         <div className="rounded-xl p-5 bg-linear-to-r from-[#00C95033] to-[#00BC7D33] border border-[#05DF724D]">
           <img src={Tick} alt="" />
           <p className={`my-2 ${FONTSIZE[30]} ${FONTWEIGHT[700]}`}>2</p>
-          <p className={`my-2 ${FONTSIZE[14]} ${FONTWEIGHT[400]}`} style={{ color: COLORS.green }}>Resolved</p>
+          <p
+            className={`my-2 ${FONTSIZE[14]} ${FONTWEIGHT[400]}`}
+            style={{ color: COLORS.green }}
+          >
+            Resolved
+          </p>
         </div>
 
         <div className="rounded-xl p-5 bg-linear-to-r from-[#2B7FFF33] to-[#00B8DB33] border border-[#51A2FF4D]">
           <img src={Graph} alt="" />
           <p className={`my-2 ${FONTSIZE[30]} ${FONTWEIGHT[700]}`}>3</p>
-          <p className={`my-2 ${FONTSIZE[14]} ${FONTWEIGHT[400]}`} style={{ color: COLORS.blue }}>Total Alerts</p>
+          <p
+            className={`my-2 ${FONTSIZE[14]} ${FONTWEIGHT[400]}`}
+            style={{ color: COLORS.blue }}
+          >
+            Total Alerts
+          </p>
         </div>
       </div>
 
-      <h2 className={`mt-10 flex items-center gap-2 ${FONTSIZE[24]} ${FONTWEIGHT[700]}`}>
+      <h2
+        className={`mt-10 flex items-center gap-2 ${FONTSIZE[24]} ${FONTWEIGHT[700]}`}
+      >
         <img src={Alert} alt="" /> Active Alerts - Maintenance Required
       </h2>
 
@@ -120,59 +139,120 @@ const EmergencyDashboard: React.FC = () => {
           <div className="flex gap-3 items-center">
             <img src={Water} alt="" />
             <div>
-              <p className={`flex items-center ${FONTSIZE[24]} ${FONTWEIGHT[700]}`}>
+              <p
+                className={`flex items-center ${FONTSIZE[24]} ${FONTWEIGHT[700]}`}
+              >
                 Water Leakage
               </p>
-              <p className={`${FONTSIZE[16]} ${FONTWEIGHT[400]}`} style={{ color: COLORS.secoundy_gray }}>Alert ID: #A2035</p>
+              <p
+                className={`${FONTSIZE[16]} ${FONTWEIGHT[400]}`}
+                style={{ color: COLORS.secoundy_gray }}
+              >
+                Alert ID: #A2035
+              </p>
             </div>
           </div>
-          <div className={`px-3 py-1 rounded-full ${FONTSIZE[14]} ${FONTWEIGHT[700]}`} style={{ backgroundColor: COLORS.orange + "33", color: COLORS.orange }}>
+          <div
+            className={`px-3 py-1 rounded-full ${FONTSIZE[14]} ${FONTWEIGHT[700]}`}
+            style={{
+              backgroundColor: COLORS.orange + "33",
+              color: COLORS.orange,
+            }}
+          >
             Medium Priority
           </div>
         </div>
 
         <div className="grid md:grid-cols-3 gap-3 mt-4 text-sm">
           <div className="bg-[#FFFFFF0D] p-3 rounded-lg">
-            <p className={`mb-2 ${FONTSIZE[12]} ${FONTWEIGHT[400]}`} style={{ color: COLORS.secoundy_gray }}>Location</p>
-            <p className={`flex gap-2 ${FONTSIZE[16]} ${FONTWEIGHT[700]}`}> <img src={Locatio} alt="" className="w-5 h-5" /> Block A - Common Area</p>
+            <p
+              className={`mb-2 ${FONTSIZE[12]} ${FONTWEIGHT[400]}`}
+              style={{ color: COLORS.secoundy_gray }}
+            >
+              Location
+            </p>
+            <p className={`flex gap-2 ${FONTSIZE[16]} ${FONTWEIGHT[700]}`}>
+              {" "}
+              <img src={Locatio} alt="" className="w-5 h-5" /> Block A - Common
+              Area
+            </p>
           </div>
 
           <div className="bg-[#FFFFFF0D] p-3 rounded-lg">
-            <p className={`mb-2 ${FONTSIZE[12]} ${FONTWEIGHT[400]}`} style={{ color: COLORS.secoundy_gray }}>Time</p>
-            <p className={`flex gap-2 ${FONTSIZE[16]} ${FONTWEIGHT[700]}`}> <img src={Clock} alt="" className="w-5 h-5" /> 08:30 PM</p>
+            <p
+              className={`mb-2 ${FONTSIZE[12]} ${FONTWEIGHT[400]}`}
+              style={{ color: COLORS.secoundy_gray }}
+            >
+              Time
+            </p>
+            <p className={`flex gap-2 ${FONTSIZE[16]} ${FONTWEIGHT[700]}`}>
+              {" "}
+              <img src={Clock} alt="" className="w-5 h-5" /> 08:30 PM
+            </p>
           </div>
 
           <div className="bg-[#FFFFFF0D] p-3 rounded-lg">
-            <p className={`mb-2 ${FONTSIZE[12]} ${FONTWEIGHT[400]}`} style={{ color: COLORS.secoundy_gray }}>Assigned</p>
-            <p className={`flex gap-2 ${FONTSIZE[16]} ${FONTWEIGHT[700]}`}> <img src={worker} alt="" className="w-5 h-5" /> Team-2</p>
+            <p
+              className={`mb-2 ${FONTSIZE[12]} ${FONTWEIGHT[400]}`}
+              style={{ color: COLORS.secoundy_gray }}
+            >
+              Assigned
+            </p>
+            <p className={`flex gap-2 ${FONTSIZE[16]} ${FONTWEIGHT[700]}`}>
+              {" "}
+              <img src={worker} alt="" className="w-5 h-5" /> Team-2
+            </p>
           </div>
         </div>
 
-        <div className={`bg-[#FFFFFF0D] p-3 rounded-lg mt-3 ${FONTSIZE[14]} ${FONTWEIGHT[400]}`}>
-          <p className={`mb-1 ${FONTSIZE[14]} ${FONTWEIGHT[400]}`} style={{ color: COLORS.secoundy_gray }}>Alert Details:</p>
+        <div
+          className={`bg-[#FFFFFF0D] p-3 rounded-lg mt-3 ${FONTSIZE[14]} ${FONTWEIGHT[400]}`}
+        >
+          <p
+            className={`mb-1 ${FONTSIZE[14]} ${FONTWEIGHT[400]}`}
+            style={{ color: COLORS.secoundy_gray }}
+          >
+            Alert Details:
+          </p>
           Water leakage detected on Floor 3. Maintenance team is working on it.
           Please avoid the area.
         </div>
 
-        <div className={`mt-3 bg-[#F0B1001A] border border-[#FDC7004D] p-3 rounded-lg ${FONTSIZE[14]} ${FONTWEIGHT[400]}`} style={{ color: COLORS.orange }}>
-          ⚠️ This leak requires maintenance team action. Please coordinate with security and admin.
+        <div
+          className={`mt-3 bg-[#F0B1001A] border border-[#FDC7004D] p-3 rounded-lg ${FONTSIZE[14]} ${FONTWEIGHT[400]}`}
+          style={{ color: COLORS.orange }}
+        >
+          ⚠️ This leak requires maintenance team action. Please coordinate with
+          security and admin.
         </div>
       </div>
 
-      <div className="rounded-2xl p-5"
-        style={{ border: "1px solid #FFFFFF33", background: "#FFFFFF0D" }}>
+      <div
+        className="rounded-2xl p-5"
+        style={{ border: "1px solid #FFFFFF33", background: "#FFFFFF0D" }}
+      >
         <div className="flex items-center gap-3">
           <img src={Note} alt="" className="w-5 h-5" />
           <h2 className={`mb-1 ${FONTSIZE[24]} ${FONTWEIGHT[700]}`}>
             Emergency Alert History
           </h2>
         </div>
-        <p className={`mb-1 ${FONTSIZE[14]} ${FONTWEIGHT[400]}`} style={{ color: COLORS.secoundy_gray }}>All emergency alerts related to maintenance</p>
+        <p
+          className={`mb-1 ${FONTSIZE[14]} ${FONTWEIGHT[400]}`}
+          style={{ color: COLORS.secoundy_gray }}
+        >
+          All emergency alerts related to maintenance
+        </p>
 
         <div className="w-full overflow-x-auto">
           <table className="min-w-[950px] w-full">
-            <thead className={`${FONTSIZE[12]} ${FONTWEIGHT[700]}`}
-              style={{ color: COLORS.secoundy_gray, borderBottom: "1px solid #ffffff1a" }}>
+            <thead
+              className={`${FONTSIZE[12]} ${FONTWEIGHT[700]}`}
+              style={{
+                color: COLORS.secoundy_gray,
+                borderBottom: "1px solid #ffffff1a",
+              }}
+            >
               <tr className="uppercase">
                 <th className="text-left py-3">Alert Type</th>
                 <th className="text-left py-3">Priority</th>
@@ -189,31 +269,58 @@ const EmergencyDashboard: React.FC = () => {
                 const s = statusStyle(item.status);
 
                 return (
-                  <tr key={index} className={FONTSIZE[14]} style={{ borderBottom: "1px solid #ffffff1a" }}>
+                  <tr
+                    key={index}
+                    className={FONTSIZE[14]}
+                    style={{ borderBottom: "1px solid #ffffff1a" }}
+                  >
                     <td className="py-3 flex items-center gap-4">
                       <div>
-                        <img src={item.img} alt=""  className="w-6 h-6"/>
+                        <img src={item.img} alt="" className="w-6 h-6" />
                       </div>
                       <div>
-                        <p className={`${FONTSIZE[16]} ${FONTWEIGHT[700]}`}>{item.type}</p> 
-                        <p className={`${FONTSIZE[12]} ${FONTWEIGHT[400]}`} style={{color: COLORS.secoundy_gray}}>{item.code}</p>
+                        <p className={`${FONTSIZE[16]} ${FONTWEIGHT[700]}`}>
+                          {item.type}
+                        </p>
+                        <p
+                          className={`${FONTSIZE[12]} ${FONTWEIGHT[400]}`}
+                          style={{ color: COLORS.secoundy_gray }}
+                        >
+                          {item.code}
+                        </p>
                       </div>
-                      </td>
+                    </td>
                     <td>
-                      <span className={`px-3 py-2 rounded-2xl ${FONTSIZE[12]} ${FONTWEIGHT[700]}`}
-                        style={{ background: p.bg, color: p.color }}>
+                      <span
+                        className={`px-3 py-2 rounded-2xl ${FONTSIZE[12]} ${FONTWEIGHT[700]}`}
+                        style={{ background: p.bg, color: p.color }}
+                      >
                         {item.priority}
                       </span>
                     </td>
-                    <td className={`${FONTSIZE[14]} ${FONTWEIGHT[700]}`} style={{ color: COLORS.blue }}>{item.location}</td>
+                    <td
+                      className={`${FONTSIZE[14]} ${FONTWEIGHT[700]}`}
+                      style={{ color: COLORS.blue }}
+                    >
+                      {item.location}
+                    </td>
                     <td style={{ color: COLORS.grey }}>
-                        <p className={`${FONTSIZE[14]} ${FONTWEIGHT[400]}`} style={{color: COLORS.primary_white}}>{item.date}</p>
-                        <p className={`${FONTSIZE[12]} ${FONTWEIGHT[400]}`}>{item.time}</p>
-                       </td>
+                      <p
+                        className={`${FONTSIZE[14]} ${FONTWEIGHT[400]}`}
+                        style={{ color: COLORS.primary_white }}
+                      >
+                        {item.date}
+                      </p>
+                      <p className={`${FONTSIZE[12]} ${FONTWEIGHT[400]}`}>
+                        {item.time}
+                      </p>
+                    </td>
                     <td style={{ color: COLORS.smalltext }}>{item.assigned}</td>
                     <td>
-                      <span className={`px-3 py-2 rounded-2xl ${FONTSIZE[12]} ${FONTWEIGHT[700]}`}
-                        style={{ background: s.bg, color: s.color}}>
+                      <span
+                        className={`px-3 py-2 rounded-2xl ${FONTSIZE[12]} ${FONTWEIGHT[700]}`}
+                        style={{ background: s.bg, color: s.color }}
+                      >
                         {item.status}
                       </span>
                     </td>
@@ -236,8 +343,12 @@ const EmergencyDashboard: React.FC = () => {
         <div className="grid md:grid-cols-2 gap-4 text-sm">
           <div className="bg-[#FFFFFF0D] p-4 rounded-xl">
             <h3 className={`mb-2 ${FONTSIZE[16]} ${FONTWEIGHT[700]}`}>
-              <img src={Wire} alt="" className="w-5 h-5 mb-3" /> Power Failure</h3>
-            <ul className={`space-y-1 list-disc list-inside ${FONTSIZE[14]} ${FONTWEIGHT[400]}`} style={{ color: COLORS.secoundy_gray }}>
+              <img src={Wire} alt="" className="w-5 h-5 mb-3" /> Power Failure
+            </h3>
+            <ul
+              className={`space-y-1 list-disc list-inside ${FONTSIZE[14]} ${FONTWEIGHT[400]}`}
+              style={{ color: COLORS.secoundy_gray }}
+            >
               <li>Stay calm</li>
               <li>Use flashlight</li>
               <li>Avoid elevators</li>
@@ -247,8 +358,13 @@ const EmergencyDashboard: React.FC = () => {
 
           <div className="bg-[#FFFFFF0D] p-4 rounded-xl">
             <h3 className={`mb-2 ${FONTSIZE[16]} ${FONTWEIGHT[700]}`}>
-              <img src={Alert} alt="" className="w-5 h-5 mb-3" /> Security Threat</h3>
-            <ul className={`space-y-1 list-disc list-inside ${FONTSIZE[14]} ${FONTWEIGHT[400]}`} style={{ color: COLORS.secoundy_gray }}>
+              <img src={Alert} alt="" className="w-5 h-5 mb-3" /> Security
+              Threat
+            </h3>
+            <ul
+              className={`space-y-1 list-disc list-inside ${FONTSIZE[14]} ${FONTWEIGHT[400]}`}
+              style={{ color: COLORS.secoundy_gray }}
+            >
               <li>Lock doors</li>
               <li>Stay inside</li>
               <li>Call security</li>
@@ -258,8 +374,12 @@ const EmergencyDashboard: React.FC = () => {
 
           <div className="bg-[#FFFFFF0D] p-4 rounded-xl">
             <h3 className={`mb-2 ${FONTSIZE[16]} ${FONTWEIGHT[700]}`}>
-              <img src={Water} alt="" className="w-5 h-5 mb-3" /> Water Leakage</h3>
-            <ul className={`space-y-1 list-disc list-inside ${FONTSIZE[14]} ${FONTWEIGHT[400]}`} style={{ color: COLORS.secoundy_gray }}>
+              <img src={Water} alt="" className="w-5 h-5 mb-3" /> Water Leakage
+            </h3>
+            <ul
+              className={`space-y-1 list-disc list-inside ${FONTSIZE[14]} ${FONTWEIGHT[400]}`}
+              style={{ color: COLORS.secoundy_gray }}
+            >
               <li>Turn off valves</li>
               <li>Avoid wet areas</li>
               <li>Inform maintenance</li>
@@ -269,8 +389,12 @@ const EmergencyDashboard: React.FC = () => {
 
           <div className="bg-[#FFFFFF0D] p-4 rounded-xl">
             <h3 className={`mb-2 ${FONTSIZE[16]} ${FONTWEIGHT[700]}`}>
-              <img src={Thunder} alt="" className="w-5 h-5 mb-3" /> Gas Leak</h3>
-            <ul className={`space-y-1 list-disc list-inside ${FONTSIZE[14]} ${FONTWEIGHT[400]}`} style={{ color: COLORS.secoundy_gray }}>
+              <img src={Thunder} alt="" className="w-5 h-5 mb-3" /> Gas Leak
+            </h3>
+            <ul
+              className={`space-y-1 list-disc list-inside ${FONTSIZE[14]} ${FONTWEIGHT[400]}`}
+              style={{ color: COLORS.secoundy_gray }}
+            >
               <li>Do not use switches</li>
               <li>Open windows</li>
               <li>Evacuate area</li>
